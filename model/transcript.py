6f457275731f84ba4e25b34500a0f46a89d631b1
vivekpawar9005 <vivek.pawar@yappes.com>
2021-12-22 20:02:21 +0530

    latest code 22 dec.

--- model/transcript.py
+++ model/transcript.py
@@ -5,75 +5,66 @@ from dominate.tags import *
 import requests
 import pdfkit
 import os
+from configs import settings
 
-import os
+
+from server import highlight
 
 class Transcript():
     def __init__(self):
         print('APi Obj created')
-    def get_html(self,data):
+        self.absolute_path=os.path.abspath(__file__)
+        if (os.path.isdir(os.path.join(settings.BASE_DIR, 'temp')) ) == False:
+            os.mkdir(os.path.join(settings.BASE_DIR, 'temp'))
+
+
+    def get_html(self, data, users):
         doc = dominate.document()
         transcriptData=data['transcriptArray']
-        print(transcriptData)
+        print("transcriptData")
+        print(transcriptData,users)
         with doc:
-            with body(style="text-align:center;width:100%;"):
-                for i in range(len(transcriptData)):
-                    with table(style="width: 90%;border: 1px solid black;border-collapse: collapse;margin-top: 5%;margin-left: auto;margin-right: auto;font-family: 'Roboto';").add(tbody()):
-                        l1=tr()
-                        with l1.add(td(style="")):
-                            div(transcriptData[i]['name'],style='padding-top:0!important;margin-block-start: 0!important;margin-block-end: 0!important;')
-                            div(transcriptData[i]['time'],style='padding-top:0!important;margin-block-start: 0!important;margin-block-end: 0!important;')
-                        with l1.add(td(style="padding-top: 5px;padding-left: 10px;padding-bottom: 10px;text-align: left;")):
-                            div(transcriptData[i]['data'],style='padding-top:0!important;margin-block-end: 0!important;')
-                    # if index+1<len(transcriptData) and transcriptData[index]['name'] != transcriptData[index+1]['name'] :
-                    #     with div(style="width:100%;display:flex;flex-direction:row"):
-                    #         with div(style="flex:0;display:flex;flex-direction:column;"):
-                    #             h3(transcriptData[index]['name'])
-                    #             p(transcriptData[index]['time'])
-                    #         with div(style="flex:1;"):
-                    #             with p():
-                    #                 if(transcriptData[index]['remark'] == 'highlight'):
-                    #                     b(transcriptData[index]['data'])
-                    #                 else:
-                    #                     p(transcriptData[index]['data'])
-                    #     index += 1
-                    # elif index+1<len(transcriptData) and transcriptData[index]['name'] == transcriptData[index+1]['name']:
-                    #     with div(style="width:100%;display:flex;flex-direction:row"):
-                    #         with div(style="flex:0;display:flex;flex-direction:column;"):
-                    #             h3(transcriptData[index]['name'])
-                    #             p(transcriptData[index]['time'])
-                    #         with div(style="flex:1;"):
-                    #             temp=transcriptData[ index : len(transcriptData)]
-                    #             mono=''
-                    #             for j in range(len(temp)):
-                    #                 try:
-                    #                     if (j+1<len(temp) and temp[j]['name'] != temp[j+1]['name']):
-                    #                         p(mono)
-                    #                         break
-                    #                     else:
-                    #                         if(temp[j]['remark']=='highlight'):
-                    #                             mono += "<b>"+temp[j]['data']+"</b>"
-                    #                         else:
-                    #                             mono += temp[j]['data']
-                    #                 except:
-                    #                     print("execption raised")
-                    #                 finally:
-                    #                     p(mono)
-                    #                     break
-  
-                    #     index += 1
+            with body():
+                h1(users['user'][0].upper()+users['user'][1:len(users['user'])]+"'s transcript",style="text-align:center;")
+                for j in range(len(transcriptData)):
+                    with div(style="display:flex;flex-direction:row;justify-content: space-evenly;border: 1px solid gainsboro;padding: 1rem;margin-bottom: 1rem;"):
+                        with div(style="flex:2; display:flex; flex-direction:column;padding-right:2rem"):
+                            h3(transcriptData[j]['name'])
+                            i(transcriptData[j]['time'],style="font-size: small;")
+                        with div(style="flex:6"):
+                            highlight_index=-1
+                            try:
+                                highlight_index=transcriptData[j]['highlightedUsers'].index(users['user'])
+                            except ValueError:
+                                pass
+                            if(highlight_index == -1):                        
+                                p(transcriptData[j]['data'])
+                            else:
+                                p(b(transcriptData[j]['data'],style="background:#fffb89"))
 
-            return doc.render()
+                    
+                    
+
+        return doc.render()
 
 
     def get_transcript(self,data):
         db = ypmongo_pool.get_db_connection()
         collection = db['room-meta']
-        query={'meetingId':data['meetingId']}
-        res=collection.find_one(query)
-        #print(res)
-        
-        print(self.get_html(res))
+        query = { 'meetingId': data['meetingId'] }
+        res = collection.find_one(query)
+        pdfkit_options = {
+        'page-size':'Letter',
+        'encoding':'utf-8', 
+        'margin-top':'1.2cm',
+        'margin-bottom':'1cm',
+        'margin-left':'1cm',
+        'margin-right':'1cm'
+        }
+        print("base dir")
+        print(settings.BASE_DIR)
+        pdfkit.from_string(self.get_html(res,data), os.path.join(settings.BASE_DIR, 'temp')+'/'+data['meetingId']+'.pdf',options=pdfkit_options)
+
         return 'pdf generated'
 
     def push_transcript_chunks(self,data):
@@ -81,12 +72,11 @@ class Transcript():
         collection = db['room-meta']
         query={'meetingId':data['meetingId']}
         res=collection.find_one(query)
-       # print(res)
-        push_data={'name':data['name'],'data':data['data'],'time':data['date'],'remark':data['remark']}
+        push_data={'name':data['name'],'data':data['data'],'time':data['date'],'highlightedUsers':data['highlightedUsers']}
         if res is None:
             collection.insert({'meetingId':data['meetingId'],'transcriptArray':[push_data]})
         else:
-            collection.update(query,{'$addToSet':{'transcriptArray':push_data}})
+            collection.update(query,{'$addToSet':{ 'transcriptArray': push_data }})
 
 
      
